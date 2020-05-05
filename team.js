730593564e2c3ed8cf2c26467a828c0c960f397b
Chris <mr.skytte@me.com>
2020-05-05 23:01:07 +0200

    team page js done

---
+++ team.js
@@ -0,0 +1,57 @@
+"use strict";
+import "@babel/polyfill";
+import createCards from "./createCards";
+
+const endpoint = "https://frontend-028f.restdb.io/rest/clients/";
+const apiKey = "5e958922436377171a0c2357";
+let client = "";
+let clientID = "";
+
+window.addEventListener("load", init);
+
+function init() {
+  client = searchURL("client");
+  clientID = searchURL("id");
+  fetchTeam();
+  setClientName(client);
+  console.log(client, clientID);
+}
+
+function searchURL(data) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const result = urlParams.get(data);
+  return result;
+}
+
+function setClientName() {
+  document.querySelector(".title>.clientname").textContent = client;
+}
+
+async function fetchTeam() {
+  const data = await fetch(`${endpoint}${clientID}`, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": apiKey,
+      "cache-control": "no-cache",
+    },
+  });
+  const response = await data.json();
+  handleData(response.members);
+}
+
+function handleData(members) {
+  members.forEach(createCards);
+  removeEmptySections();
+}
+
+function removeEmptySections() {
+  const sections = document.querySelectorAll("section");
+  sections.forEach((section) => {
+    console.log(section.childNodes[5].childNodes.length);
+    if (section.childNodes[5].childNodes.length === 0) {
+      section.classList.add("hide");
+    }
+  });
+}
